f8669735a9284490a06c0556e601452841f9c615
Todd Stoneberg <todd.stoneberg@gmail.com>
2020-09-24 20:38:28 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,7 +6,7 @@ class Dog
   
   def initialize(name)
     @name = name
-    @@all << self
+    self.save
   end
   
   def self.all
@@ -24,7 +24,7 @@ class Dog
   end
   
   def save
-    name.initialize
+    @@all << self
   end
 
   
